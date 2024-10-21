ed39e8537467033fa2fbeac665250f687b602327
S-Anbu <anbarasans750@gmail.com>
2024-10-21 11:08:14 +0530

    added buttons  for payment gateway

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/AADHI-Engineering-Works/',
+  base: process.env.NODE_ENV === 'production' ?'/AADHI-Engineering-Works/': '/',
   build: {
     chunkSizeWarningLimit: 1600,
+    outDir: 'dist',
   },
 
 })
