bda11520cdbf4dec434300154e6c1425054c31b6
S-Anbu <anbarasans750@gmail.com>
2024-10-21 11:32:30 +0530

    netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ?'/AADHI-Engineering-Works/': '/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1600,
     outDir: 'dist',
