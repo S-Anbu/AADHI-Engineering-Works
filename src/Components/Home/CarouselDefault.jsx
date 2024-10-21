486bea86e65c098979526392888a681e10165bab
S-Anbu <anbarasans750@gmail.com>
2024-10-21 12:37:04 +0530

    netlify

--- src/Components/Home/CarouselDefault.jsx
+++ src/Components/Home/CarouselDefault.jsx
@@ -14,49 +14,62 @@ export function CarouselDefault() {
       title: "Welcome to AADHI Engineering Works",
       subtitle1: "#1 Welding",
       subtitle2: "Services Provider",
-      buttonText: "our services"
+      buttonText: "our services",
+      href:'#Welding'
     },
     {
       image: slider2,
       title: "10 Years of high-quality welding services",
       subtitle1: "Certified Welding",
       subtitle2: "Of Any Complexity",
-      buttonText: "our services"
+      buttonText: "our services",
+      href:'#Welding'
+
     },
     {
       image: slider3,
       title: "Providing reliable & innovative welding services",
       subtitle1: "Industry Leading",
       subtitle2: "Welding Solutions",
-      buttonText: "our services"
+      buttonText: "our services",
+      href:'#Welding'
+
     },
     {
       image: slider4,
       title: "24/7 Emergency Electrical Support",
       subtitle1: " Fast, dependable assistance",
       subtitle2: " when you need it most",
-      buttonText: "our services"
+      buttonText: "our services",
+      href:'#Electrical'
+
     },
     {
       image: slider5,
       title: "Expert Electrical Installations",
       subtitle1: " Safe, reliable solutions for",
       subtitle2: " homes and businesses",
-      buttonText: "our services"
+      buttonText: "our services",
+      href:'#Electrical'
+
     },
     {
       image: slider6,
       title: "Professional Plumbing Services",
       subtitle1: "Industry Leading",
       subtitle2: "Welding Solutions",
-      buttonText: "our services"
+      buttonText: "our services",
+      href:'#Plumbing'
+
     },
     {
       image: slider7,
       title: "Bathroom & Kitchen Installations",
       subtitle1: "Upgrade your space with",
       subtitle2: "modern plumbing fixtures",
-      buttonText: "our services"
+      buttonText: "our services",
+      href:'#Plumbing'
+
     }
   ]
 
@@ -93,9 +106,9 @@ export function CarouselDefault() {
             <p className="text-3xl sm:text-5xl font-bold">{slide.subtitle1}</p>
             <p className="text-3xl sm:text-5xl font-bold">{slide.subtitle2}</p>
             </div>
-            <button className="uppercase px-4 py-2 text-black rounded-md font-bold bg-[#fce200] bg-opacity-90 ">
+            <a href={slide.href}  className="uppercase px-4 py-2 text-black rounded-md font-bold bg-[#fce200] bg-opacity-90 ">
               {slide.buttonText}
-            </button>
+            </a>
           </div>
         </div>
       ))}
